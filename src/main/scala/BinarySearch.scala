21aa4422837d1e5da014e767c49d391f2d51ec27
Priscila Carvalho <priscila.carvalho@99taxis.com>
2018-02-17 13:53:26 -0200

    implements a recursive functio to BinarySearch.scala

--- src/main/scala/BinarySearch.scala
+++ src/main/scala/BinarySearch.scala
@@ -1,31 +1,26 @@
 object BinarySearch {
-  import scala.util.control.Breaks._
 
-  def binarySearch(list: Array[Int], item: Int): Option[Int] = {
-    var low = 0
-    var high = list.length - 1
+  def recursiveBinarySearch(list: Array[Int], item: Int): Option[Int] = {
+    def binarySearch(low: Int, high: Int): Option[Int] = {
+      val middlePosition = (low + high) / 2
+      val positionValue = list(middlePosition)
 
-    var value = None
-    breakable {
-      while (low <= high) {
-        var middle = (low + high) / 2
-        val guess = list(middle)
-        if (guess == item) {
-          return Some(middle)
-        }
-        else if (guess > item) high = middle - 1
-        else low = middle + 1
-      }
+      if(positionValue == item)
+        Some(middlePosition)
+      else if (low <= high) {
+        if(positionValue > item) binarySearch(low, high = (middlePosition - 1))
+          else binarySearch(low = (middlePosition + 1), high)
+      } else None
     }
-    value
+    binarySearch(low = 0, high = (list.length - 1))
   }
 
 
   def main(args: Array[String]): Unit = {
     val list = Array(1,3,5,7,9)
 
-    println(binarySearch(list, 3))
-    println(binarySearch(list, -1))
+    println(recursiveBinarySearch(list, 3))
+    println(recursiveBinarySearch(list, -1))
   }
 
 }
