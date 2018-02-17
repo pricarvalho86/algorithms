014980ea8d29945b4bdfb29c8bde4c4afae0ba03
Priscila Carvalho <priscila.carvalho@99taxis.com>
2018-02-17 14:17:48 -0200

    implements a recursive functio to BinarySearch.py

--- src/main/python/BinarySearch.py
+++ src/main/python/BinarySearch.py
@@ -1,20 +1,23 @@
-def binary_search(list, item):
+def recursive_binary_search(list, item):
     low = 0
     high = len(list) - 1
+    return binary_search(low, high, list, item)
 
-    while low <= high:
-        middle = (low + high) / 2
-        guess = list[middle]
 
-        if guess == item:
-            return middle
+def binary_search(low, high, list, item):
+    middle = (low + high) / 2
+    guess = list[middle]
+
+    if guess == item:
+        return middle
+    if low <= high:
         if guess > item:
-            high = middle - 1
+            return binary_search(low, middle - 1, list, item)
         else:
-            low = middle + 1
+            return binary_search(middle + 1, high, list, item)
     return None
 
 my_list = [1,3,5,7,9]
 
-print binary_search(my_list, 7)
-print binary_search(my_list, -1)
+print recursive_binary_search(my_list, 3)
+print recursive_binary_search(my_list, -1)
