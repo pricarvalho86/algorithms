64a465715f319dd43bdf5f077e5246c0a650240d
Priscila Carvalho <priscila.carvalho@99taxis.com>
2018-02-17 14:58:58 -0200

    calculates the max iteration steps to find element with binary search

--- src/main/python/BinarySearch.py
+++ src/main/python/BinarySearch.py
@@ -1,10 +1,10 @@
-def recursive_binary_search(list, item):
+def binary_search(list, item):
     low = 0
     high = len(list) - 1
-    return binary_search(low, high, list, item)
+    return recursive_binary_search(low, high, list, item)
 
 
-def binary_search(low, high, list, item):
+def recursive_binary_search(low, high, list, item):
     middle = (low + high) / 2
     guess = list[middle]
 
@@ -12,12 +12,24 @@ def binary_search(low, high, list, item):
         return middle
     if low <= high:
         if guess > item:
-            return binary_search(low, middle - 1, list, item)
+            return recursive_binary_search(low, middle - 1, list, item)
         else:
-            return binary_search(middle + 1, high, list, item)
+            return recursive_binary_search(middle + 1, high, list, item)
     return None
+
+
+def calculate_log(elementsSize, iteractions = 0):
+    result = elementsSize / 2
+
+    if result != 0:
+        return calculate_log(result, iteractions + 1)
+
+    return iteractions
 
 my_list = [1,3,5,7,9]
 
-print recursive_binary_search(my_list, 3)
-print recursive_binary_search(my_list, -1)
+print binary_search(my_list, 3)
+print binary_search(my_list, -1)
+
+print calculate_log(128)
+print calculate_log(128 * 2)
--- src/main/scala/BinarySearch.scala
+++ src/main/scala/BinarySearch.scala
@@ -1,26 +1,39 @@
+import scala.annotation.tailrec
+
 object BinarySearch {
 
-  def recursiveBinarySearch(list: Array[Int], item: Int): Option[Int] = {
-    def binarySearch(low: Int, high: Int): Option[Int] = {
+  def binarySearch(list: Array[Int], item: Int): Option[Int] = {
+    def recursiveBinarySearch(low: Int, high: Int): Option[Int] = {
       val middlePosition = (low + high) / 2
       val positionValue = list(middlePosition)
 
       if(positionValue == item)
         Some(middlePosition)
       else if (low <= high) {
-        if(positionValue > item) binarySearch(low, high = (middlePosition - 1))
-          else binarySearch(low = (middlePosition + 1), high)
+        if(positionValue > item) recursiveBinarySearch(low, high = (middlePosition - 1))
+          else recursiveBinarySearch(low = (middlePosition + 1), high)
       } else None
     }
-    binarySearch(low = 0, high = (list.length - 1))
+    recursiveBinarySearch(low = 0, high = (list.length - 1))
+  }
+
+  @tailrec
+  def calculateLog(listSize: Int, iteractions: Int = 0): Int = {
+    val result = listSize / 2
+    if(result != 0)
+      calculateLog(result, iteractions + 1)
+    else iteractions
   }
 
 
   def main(args: Array[String]): Unit = {
     val list = Array(1,3,5,7,9)
 
-    println(recursiveBinarySearch(list, 3))
-    println(recursiveBinarySearch(list, -1))
+    println(binarySearch(list, 3))
+    println(binarySearch(list, -1))
+
+    println(calculateLog(128))
+    println(calculateLog(128 * 2))
   }
 
 }
