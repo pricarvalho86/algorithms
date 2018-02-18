02594a9f136dbe6616572e96ce0c1085f7d5154c
Priscila Carvalho <priscila.carvalho@99taxis.com>
2018-02-18 08:52:51 -0300

    fix in inner function recursiveLowerValue in Sorting.scala

--- src/main/scala/Sorting.scala
+++ src/main/scala/Sorting.scala
@@ -5,14 +5,16 @@ object Sorting {
   def findLowerValueFrom(list: Array[Int], index: Int = 0): Int = {
     @tailrec
     def recursiveLowerValue(value: Int, index: Int): Int = {
-      if(index < (list.length - 1)) {
-        val currentValue = list(index)
-        if(currentValue < value) {
-          recursiveLowerValue(currentValue, index+1)
-        } else recursiveLowerValue(value, index+1)
+      if(index < list.length) {
+        val nextValue = list(index)
+        if(nextValue < value) {
+          recursiveLowerValue(nextValue, index+1)
+        } else {
+          recursiveLowerValue(value, index + 1)
+        }
       } else value
     }
-    recursiveLowerValue(list(index), index)
+    recursiveLowerValue(list(index), index+1)
   }
 
   def sorting(list: Array[Int]) = {
@@ -20,7 +22,8 @@ object Sorting {
     def recursiveSorting(currentList: Array[Int], sortedlist: Array[Int]): Array[Int] = {
       if(sortedlist.length < list.length) {
         val lowerValue = findLowerValueFrom(currentList)
-        val otherValues = currentList.filterNot(v => v == lowerValue)
+        val i = currentList.indexOf(lowerValue)
+        val otherValues = currentList.zipWithIndex.filterNot(_._2 == i).map(_._1)
         val array = sortedlist.:+(lowerValue)
         recursiveSorting(otherValues, array)
       } else sortedlist
@@ -29,8 +32,9 @@ object Sorting {
   }
 
   def main(args: Array[String]): Unit = {
-    val list = Array(5,3,6,2,10)
-    println(sorting(list).mkString)
+    val list = Array(5,3,6,2,10,1,1)
+
+    println(sorting(list).mkString(","))
   }
 
 }
